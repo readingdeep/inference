a30aeea5db8597efb1357991a7ae4581bced737b
Leonardo Rosenberg <nudel.rosenberg@gmail.com>
2021-06-02 17:19:10 +0300

    first version predict

---
+++ inference.py
@@ -0,0 +1,84 @@
+import json
+import pickle
+import os
+import nltk
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify
+
+LABELS = "model/labeled_words.pkl"
+COUNT_VECTORIZER = "model/count_vectorizer.pkl"
+VOCAB = "model/vocab.pkl"
+NEWLINE = '\n'
+EMPTY = ''
+REMOVE_PUNCTUATION = r'[^\w\s]'
+TEXT_IDX = -1
+WHITESPACE = " "
+NUM_LABELS = 10
+ARRAY_INSIDE_ARRAY_IDX = 0
+LABEL_IDX = 1
+
+app = Flask(__name__)
+
+
+def clean_data(data):
+    """
+    TODO
+    """
+    df = data.copy()
+    df = df.iloc[:, TEXT_IDX].str.replace(NEWLINE, EMPTY)
+    df = df.iloc[:, TEXT_IDX].str.replace(REMOVE_PUNCTUATION, EMPTY, regex=True).str.lower()
+    df = df.map(nltk.word_tokenize)
+    return df
+
+
+def replace_if_not_in_vocab(lst_token, vocab):
+    result = []
+    for t in lst_token:
+        try:
+            idx = vocab.index(t)
+            result.append(t)
+        except ValueError:
+            result.append('unk')
+    return result
+
+
+@app.route('/predict', methods=['POST'])
+def predict(label, cv, vocab):
+    params = json.loads(request.get_json())
+    X = pd.DataFrame(params)
+    clean_X = clean_data(X)
+    y_pred = np.zeros(clean_X.shape[0]).astype(str)
+    for i, row in clean_X.iterrows():
+        paragraph = WHITESPACE.join(replace_if_not_in_vocab(row))
+        X_cv = cv.transform([paragraph]).toarray()
+        counter = label.iloc[:, :NUM_LABELS].values.T @ X_cv[ARRAY_INSIDE_ARRAY_IDX]
+        y_pred[i] = label.columns[np.argmax(counter)][LABEL_IDX]
+    response = y_pred.tolist()
+    return json.dumps(response)
+
+
+def main():
+    """
+    """
+
+    with open(LABELS, 'rb') as pkl_file:
+        labeled_words = pickle.load(pkl_file)
+    with open(COUNT_VECTORIZER, 'rb') as pkl_file:
+        cv = pickle.load(pkl_file)
+    with open(VOCAB, 'rb') as pkl_file:
+        vocab = pickle.load(pkl_file)
+
+    # Run flask
+    port = int(os.environ.get('PORT'))
+    # Running on Heroku
+    if port:
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
+    # Running locally
+    else:
+        app.run(host='0.0.0.0')
+
+
+
+if __name__ == '__main__':
+    main()
